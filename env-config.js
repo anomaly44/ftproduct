1065efc5b7d601ad385dacfee4a72f6c4452e608
Rob <r.indesteege@gmail.com>
2017-06-18 20:43:36 +0200

    fix ip's

--- env-config.js
+++ env-config.js
@@ -1,5 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
+console.log('Setting "env" variables');
 
 module.exports = {
-  'API_URL': isProd ? 'https://api.example.com' : 'http://localhost:3333/v1'
-}
+  'API_URL': isProd ? 'http://api.fixtrack.be/v1' : 'http://localhost:3333/v1'
+};
