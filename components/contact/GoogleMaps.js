91a60e9cb0387176b7b766975018a3d83be9fbfe
Rob Indesteege <rob.indesteege@icapps.com>
2019-03-25 23:12:58 +0100

    update coordinates

--- components/contact/GoogleMaps.js
+++ components/contact/GoogleMaps.js
@@ -1,25 +1,24 @@
-import GoogleMap from 'google-map-react';
-import config from '../../config'
+import GoogleMap from "google-map-react";
+import config from "../../config";
 
-const coordinates = { lat: 50.922367, lng: 5.372040 };
+const coordinates = { lat: 50.939636, lng: 5.339189 };
 
-const renderMarkers = ({map, maps}) =>  {
-   let marker = new maps.Marker({
+const renderMarkers = ({ map, maps }) => {
+  let marker = new maps.Marker({
     position: coordinates,
     map,
-    title: 'FixTrack'
+    title: "FixTrack"
   });
 };
 
 export default () => (
-  <div style={{ height: 350, width: '100%' }}>
+  <div style={{ height: 350, width: "100%" }}>
     <GoogleMap
       bootstrapURLKeys={{ key: config.googleMapsApiKey }}
       defaultCenter={coordinates}
       defaultZoom={14}
       onGoogleApiLoaded={renderMarkers}
       yesIWantToUseGoogleMapApiInternals
-    >
-    </GoogleMap>
+    />
   </div>
 );
